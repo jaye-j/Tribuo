5a040a87edb27ed10ae8a6adac9fae8f75954a55
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-03-17 18:57:29 -0500

    registration form database interaction

--- controllers/registration.js
+++ controllers/registration.js
@@ -1,8 +1,42 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const bodyParser = require("body-parser");
+const bcrypt = require("bcryptjs");
+let db = require("./models");
 
-router.get('/registration', (req, res) => {
-  res.render('registration');
+router.get("/registration", (req, res) => {
+  res.render("registration");
+});
+
+router.use(bodyParser.urlencoded({ extended: false }));
+
+router.post("/registration", (req, res) => {
+  let employee_first_name = req.body.first_name;
+  let employee_last_name = req.body.last_name;
+  let employee_email_address = req.body.email_address;
+  let departmentID = req.body.departmentID;
+  let employee_password = req.body.password;
+
+  let passwordEncypted = bcrypt.hashSync(employee_password, 8);
+
+  db.employees
+    .create({
+      employee_first_name,
+      employee_last_name,
+      employee_email_address,
+      departmentID,
+      employee_password: passwordEncypted
+    })
+    .then(newEmployee => {
+      console.log(newEmployee);
+      res.redirect("/login");
+    })
+    .catch(error => {
+      console.log(error);
+      res.render("registration", {
+        error
+      });
+    });
 });
 
 module.exports = router;
