f850c11e52153a86860bbf8bd29f7a71ee478690
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-03-22 18:56:14 -0500

    working real time task additions

--- app.js
+++ app.js
@@ -1,40 +1,53 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 2020;
-const bodyParser = require('body-parser');
-let db = require('./models');
-let sessions = require('express-session');
-let cookieParser = require('cookie-parser');
+const bodyParser = require("body-parser");
+let db = require("./models");
+let sessions = require("express-session");
+let cookieParser = require("cookie-parser");
+let io = require("socket.io")();
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.use(cookieParser());
 app.use(
   sessions({
-    secret: 'Tribuo',
+    secret: "Tribuo",
     cookie: { secure: false, maxAge: 5 * 24 * 60 * 60 * 1000 }
   })
 );
 
 // db.departments.destroy({ where: { id: 2 } });
-// for (let index = 37; index < 54; index++) {
+// for (let index = 75; index < 79; index++) {
 //   db.tasks.destroy({ where: { id: `${index}` } });
 // }
 
-app.use(require('./controllers/login'));
-app.use(require('./controllers/employee'));
-app.use(require('./controllers/manager'));
-app.use(require('./controllers/registration'));
-app.use(require('./controllers/error'));
-app.use(require('./controllers/login'));
-app.use(require('./controllers/admindashboard'));
-app.use(require('./controllers/adminlogin'));
-app.use(require('./controllers/aboutus'));
+app.use(require("./controllers/login"));
+app.use(require("./controllers/employee"));
+app.use(require("./controllers/manager"));
+app.use(require("./controllers/registration"));
+app.use(require("./controllers/error"));
+app.use(require("./controllers/login"));
+app.use(require("./controllers/admindashboard"));
+app.use(require("./controllers/adminlogin"));
+app.use(require("./controllers/aboutus"));
 
-app.listen(PORT, () => {
-  console.log('Server is listening on port 2020.');
+let server = app.listen(PORT, () => {
+  console.log("Server is listening on port 2020.");
+});
+
+io.attach(server);
+io.on("connection", socket => {
+  console.log("a user has connected");
+  socket.on("disconnect", () => {
+    console.log("a user has disconnected");
+  });
+  socket.on("new task", taskData => {
+    console.log("recieved new task on server");
+    io.emit("new task", taskData);
+  });
 });
--- public/js/employee.js
+++ public/js/employee.js
@@ -1,21 +1,45 @@
-let addTaskForm = document.querySelector('.add-task-form');
-let taskContainer = document.querySelector('.task-container');
-let claimTaskButton = document.querySelectorAll('.claim-task');
+let addTaskForm = document.querySelector(".add-task-form");
+let taskContainer = document.querySelector(".task-container");
+let claimTaskButton = document.querySelectorAll(".claim-task");
+const socket = io();
 
-addTaskForm.addEventListener('submit', e => {
+addTaskForm.addEventListener("submit", e => {
   e.preventDefault();
-  let taskTitle = document.querySelector('#task_title');
-  let taskInstruction = document.querySelector('#task_instruction');
+  let taskTitle = document.querySelector("#task_title");
+  let taskInstruction = document.querySelector("#task_instruction");
   console.log(taskTitle.value);
   console.log(taskInstruction.value);
-  fetch('/employee', {
-    method: 'POST',
+  let taskData = {
+    task_title: taskTitle.value,
+    task_instruction: taskInstruction.value
+  };
+  console.log(taskData);
+  socket.emit("new task", taskData);
+  fetch("/employee", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json"
     },
     body: JSON.stringify({
       taskTitle: taskTitle.value,
       taskInstruction: taskInstruction.value
     })
   });
+});
+
+socket.on("new task", taskData => {
+  console.log("client received new task");
+  let existingHTML = taskContainer.innerHTML;
+  console.log(existingHTML);
+  let output = "";
+  output += `<div class="task">`;
+  output += `<h1 class="task-div">${taskData.task_title}</h1>`;
+  output += `<div>${taskData.task_instruction}</div>`;
+  output += `<button class="claim-task">`;
+  output += `Claim Task`;
+  output += `</button>`;
+  output += `</div>`;
+  let updatedHTML = existingHTML + output;
+  console.log(updatedHTML);
+  taskContainer.innerHTML = updatedHTML;
 });
