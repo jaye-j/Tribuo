2cd5d6f05597efab39d0efc437cbba6e12abcd97
Austin Denny <austinbdenny@gmail.com>
2020-03-22 14:30:58 -0500

    Fixed Styling and populated manager views.

--- controllers/manager.js
+++ controllers/manager.js
@@ -1,8 +1,31 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const db = require('../models');
 
-router.get("/manager", (req, res) => {
-  res.render("manager");
+router.get('/manager', (req, res) => {
+  let department_id = req.session.department_id;
+  let employee_first_name = req.session.employee_first_name;
+  let employee_last_name = req.session.employee_last_name;
+  db.tasks
+    .findAll({
+      where: { department_id: department_id }
+    })
+    .then(results => {
+      // console.log(results[0].task_title);
+      let taskInfo = results;
+      db.departments
+        .findAll({ where: { id: department_id } })
+        .then(departmentsresult => {
+          // console.log(departmentsresult);
+          let department_title = departmentsresult[0].department_title;
+          res.render('manager', {
+            taskInfo: taskInfo,
+            employee_first_name: employee_first_name,
+            employee_last_name: employee_last_name,
+            department_title: department_title
+          });
+        });
+    });
 });
 
 module.exports = router;
--- public/js/localadmindashboard.js
+++ public/js/localadmindashboard.js
@@ -1,15 +1,15 @@
 function getUrlParameter(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
   var results = regex.exec(location.search);
   return results === null
-    ? ""
-    : decodeURIComponent(results[1].replace(/\+/g, " "));
+    ? ''
+    : decodeURIComponent(results[1].replace(/\+/g, ' '));
 }
-if (getUrlParameter("name").length > 0) {
+if (getUrlParameter('name').length > 0) {
   alert(
-    `${getUrlParameter("name")} is now the manager of ${getUrlParameter(
-      "department"
+    `${getUrlParameter('name')} is now the manager of the ${getUrlParameter(
+      'department'
     )}.`
   );
 }
